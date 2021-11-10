0cae26462432a6ed668409f20bfef4258fbb72a2
Will Boyd <will@codersblock.com>
2021-11-10 16:46:29 -0500

    Don't really need a stylesheet

--- page.js
+++ page.js
@@ -1,6 +1,6 @@
+document.querySelector('button').addEventListener('click', ping);
+
 function ping() {
   console.log('button clicked!');
   fetch('/.netlify/functions/heart-light');
 }
-
-document.querySelector('button').addEventListener('click', ping);
